b78fbb2b3f1e10f9a81cba57096facea031331dc
alexmortelsison <alexmortelsison@gmail.com>
2024-03-30 14:27:44 +0900

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Alex Sison on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
